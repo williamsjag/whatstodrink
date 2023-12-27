47b7e0b7405b5d5bc16d35ddcd31c2bff2c56e00
williamsjag <117761747+williamsjag@users.noreply.github.com>
2023-12-27 20:24:18 +0100

    update 12/26/23 lots of progress, finished add cocktail

--- cocktails/TODO.md
+++ cocktails/TODO.md
@@ -9,24 +9,30 @@ Built manage stock page
 Made add ingredient add to database
 Built add cocktail page
 Built add ingredient modal for add cocktail page
-
+Made add ingredient modal refresh the ingredient select items in add cocktail
+Made more ingredients button to copy ingredient select menu and made modal work
+Made Manage stock update database - post method of manage stock
 
 
 
 ## TODO
 
-make select ingredient open a new ingredient select in add cocktail
-make add ingredient modal refresh the ingredient select items in add cocktail
-Make Manage stock update database - post method of manage stock
+
+
 make amounts dialogue for each ingredient after add cocktail is submitted
 Make add cocktail add to database - post method of add cocktail
 
 
+Make page for cocktail queries
+Make SQL query for cocktails you can make
+Make SQL query for cocktails you can almost make
 
 
 
 ## Future Features
 
+Rename button in manage ingredients that makes a modal to rename and update database for that id
 add tags tables: https://stackoverflow.com/questions/51128832/what-is-the-best-way-to-design-a-tag-based-data-table-with-sqlite
 select units imperial/metric
 Make 'modify ingredient' with rename and delete
+make ratings feature
--- cocktails/app.py
+++ cocktails/app.py
@@ -1,7 +1,9 @@
 import os
 
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+import sqlite3
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+import logging
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -10,6 +12,8 @@ from helpers import apology, login_required
 # Configure application
 app = Flask(__name__)
 
+app.logger.setLevel(logging.INFO)
+
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -17,6 +21,7 @@ Session(app)
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///cocktails.db")
+app.config['db'] = '/cocktails.db'
 
 
 @app.after_request
@@ -31,11 +36,16 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index():
-
+    cocktails = db.execute("SELECT c.id, c.name FROM cocktails c JOIN amounts a ON c.id = a.cocktail_id JOIN ingredients i ON a.ingredient_id = i.id JOIN users u ON i.user_id = u.id WHERE i.stock = 'on' AND u.id = ? GROUP BY c.id HAVING COUNT(*) = (SELECT COUNT(*) FROM amounts WHERE cocktail_id = c.id)", session["user_id"])
+    ingredients = db.execute("SELECT * FROM ingredients WHERE user_id = ?", session["user_id"])
+    # try:
+        #amounts = db.execute("SELECT * FROM amounts WHERE cocktail_id IN (SELECT c.id FROM cocktails c JOIN amounts a ON c.id = a.cocktail_id JOIN ingredients i ON a.ingredient_id = i.id JOIN users u ON i.user_id = u.id WHERE i.stock = 'on' AND u.id = ? GROUP BY c.id HAVING COUNT(*) = (SELECT COUNT(*) FROM amounts WHERE cocktail_id = c.id)", session["user_id"])
+    #except Exception as e:
+        #print(f"Error executing query: {e}")
     return render_template(
-        "index.html",
+        "index.html", cocktails=cocktails, ingredients=ingredients,
     )
-
+  
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -142,7 +152,7 @@ def addingredient():
 
         # Query database for ingredient
         rows = db.execute(
-            "SELECT * FROM ingredients WHERE name = ?", request.form.get("ingredientname")
+            "SELECT * FROM ingredients WHERE name = ? AND user_id = ?", request.form.get("ingredientname"), session["user_id"]
         )
 
         # Ensure username exists and password is correct
@@ -182,7 +192,7 @@ def ingredientmodal():
 
         # Query database for ingredient
         rows = db.execute(
-            "SELECT * FROM ingredients WHERE name = ?", request.form.get("ingredientname")
+            "SELECT * FROM ingredients WHERE name = ? AND user_name = ?", request.form.get("ingredientname", session["user_id"])
         )
 
         # Ensure username exists and password is correct
@@ -198,36 +208,118 @@ def ingredientmodal():
                 request.form.get("stock"),
             )
 
+    
 
-        return render_template(
-            "addcocktail.html"
-        )
+        return redirect(url_for("addcocktail"))
       # User reached route via GET (as by clicking a link or via redirect)
      else:
         return render_template(
             "addingredient.html"
         )
 
-@app.route("/managestock", methods=["GET", "POST"])
+@app.route("/manageingredients", methods=["GET", "POST"])
 @login_required
-def managestock():
-
+def manageingredients():
+    
     if request.method =="GET":
-        ingredients = db.execute("SELECT name, type, stock FROM ingredients")
-        types = db.execute("SELECT DISTINCT type FROM ingredients")
+        ingredients = db.execute("SELECT id, name, type, stock FROM ingredients WHERE user_id = ?", session["user_id"])
+        types = db.execute("SELECT DISTINCT type FROM ingredients WHERE user_id = ?", session["user_id"])
 
         return render_template(
-            "managestock.html", ingredients=ingredients, types=types
+            "manageingredients.html", ingredients=ingredients, types=types
         )
+    elif request.method =="POST":
+        # set all stock to off
+        db.execute(
+            "UPDATE ingredients SET stock = 0 WHERE user_id = ?",
+            session["user_id"]
+        )
+        # turn checked stock on
+        for key, value, in request.form.items():
+            if key.startswith('stock_'):
+                ingredient_name = key.replace('stock_', '')
+                ingredient_id = request.form.get(f'id_{ingredient_name}')
+
+                db.execute(
+                    "UPDATE ingredients SET stock = ? WHERE id = ? AND user_id = ?",
+                    value,
+                    ingredient_id,
+                    session["user_id"]
+                )
+            
+                
+        return redirect(url_for(
+            "manageingredients"
+        ))
+
+
 
 @app.route("/addcocktail", methods=["GET", "POST"])
 @login_required
 def addcocktail():
 
     if request.method=="GET":
-        ingredients = db.execute("SELECT name, type FROM ingredients")
-        types = db.execute("SELECT DISTINCT type FROM ingredients")
+        ingredients = db.execute("SELECT name, type FROM ingredients WHERE user_id = ? ORDER BY name ASC", session["user_id"])
+        types = db.execute("SELECT DISTINCT type FROM ingredients WHERE user_id = ?", session["user_id"])
 
         return render_template(
             "addcocktail.html", ingredients=ingredients, types=types
         )
+    
+    else:
+        name = request.form.get('name')
+        build = request.form.get('build')
+        source = request.form.get('source')
+        family = request.form.get('family')
+        ingredients = request.form.getlist('ingredient')
+        db.execute(
+            "INSERT INTO cocktails (name, build, source, family, user_id) VALUES(?, ?, ?, ?, ?)",
+            name,
+            build,
+            source,
+            family,
+            session["user_id"]
+            )
+
+        cocktail_id = db.execute("SELECT id from cocktails WHERE name = ? AND user_id = ?", name, session["user_id"])[0]['id']
+
+        return render_template(
+            "amounts.html", ingredients=ingredients, cocktail_id=cocktail_id
+        )
+    
+@app.route("/amounts", methods=["GET", "POST"])
+@login_required
+def amounts():
+    # reached via post
+    if request.method == "POST":
+        for key, value, in request.form.items():
+            if key.startswith('amount_'):
+                cocktail_id = request.form.get('cocktail_id')
+                ingredient_name = key.replace('amount_', '')
+                amount = value
+                ingredient_id = (db.execute("SELECT name, id FROM ingredients WHERE name = ? AND user_id = ?", ingredient_name, session["user_id"]))[0]['id']
+                print(f"cid: {cocktail_id} ingname: {ingredient_name} amount: {amount} ing_id: {ingredient_id}")
+                db.execute(
+                    "INSERT INTO amounts (cocktail_id, ingredient_id, amount, user_id) VALUES(?, ?, ?, ?)",
+                    cocktail_id,
+                    ingredient_id,
+                    amount,
+                    session["user_id"])
+    
+    return render_template(
+        "cocktailsuccess.html"
+    )
+
+@app.route("/accordion")
+def accordion():
+    return render_template(
+        "accordion.html"
+    )
+
+
+
+    
+# @app.route('/debug', methods=["POST"])
+    # def debug():
+         #    print(f'Name: {ingredient_name} ID: {ingredient_id}, Checkbox: {value}')
+
