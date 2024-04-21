8d586e76a7cc031fdb112a2d6311e1d0e3058d7c
williamsjag <117761747+williamsjag@users.noreply.github.com>
2024-04-21 15:09:05 +0200

    Add current_app context to run.py

--- whatstodrink/run.py
+++ whatstodrink/run.py
@@ -1,10 +1,11 @@
 from whatstodrink.__init__ import create_app, db
 from socket import gethostname
+from flask import current_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    with app.app_context():
+    with current_app.app_context():
         db.create_all()
     if 'liveconsole' not in gethostname():
         app.run()
