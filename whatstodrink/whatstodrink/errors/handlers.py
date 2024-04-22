f250ba4d28847367472c237fff95413ef48b5122
williamsjag <117761747+williamsjag@users.noreply.github.com>
2024-04-22 19:46:23 +0200

    Comment out db related errorhandlers for troubleshooting

--- whatstodrink/whatstodrink/errors/handlers.py
+++ whatstodrink/whatstodrink/errors/handlers.py
@@ -16,10 +16,10 @@ def error_403(error):
 def error_500(error):
     return render_template('errors/500.html'), 500
 
-@errors.app_errorhandler(OperationalError)
-def error_500(error):
-    return render_template('errors/database_error.html'), 500
+# @errors.app_errorhandler(OperationalError)
+# def error_500(error):
+#     return render_template('errors/database_error.html'), 500
 
-@errors.app_errorhandler(PendingRollbackError)
-def error_500(error):
-    return render_template('errors/database_error.html'), 500
+# @errors.app_errorhandler(PendingRollbackError)
+# def error_500(error):
+#     return render_template('errors/database_error.html'), 500
