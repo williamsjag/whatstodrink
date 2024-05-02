b7172ea2e3d73b58b847305d7818615b04debc52
Jackson Williams <117761747+williamsjag@users.noreply.github.com>
2024-05-02 16:32:07 +0200

    Try commenting out @app.after_request

--- whatstodrink/whatstodrink/__init__.py
+++ whatstodrink/whatstodrink/__init__.py
@@ -43,13 +43,13 @@ def create_app():
     # with app.app_context():
     #     db.create_all()
 
-    @app.after_request
-    def after_request(response):
-        """Ensure responses aren't cached"""
-        response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-        response.headers["Expires"] = 0
-        response.headers["Pragma"] = "no-cache"
-        return response
+    # @app.after_request
+    # def after_request(response):
+    #     """Ensure responses aren't cached"""
+    #     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    #     response.headers["Expires"] = 0
+    #     response.headers["Pragma"] = "no-cache"
+    #     return response
     
     return app
     
