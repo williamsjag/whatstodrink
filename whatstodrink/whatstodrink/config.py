f0e72d86c0d145b7395cfc9fabccc6feda69303b
Jackson Williams <117761747+williamsjag@users.noreply.github.com>
2024-05-03 15:30:13 +0200

    Fix resetpw form for PA

--- whatstodrink/whatstodrink/config.py
+++ whatstodrink/whatstodrink/config.py
@@ -6,6 +6,8 @@ class Config:
     SESSION_TYPE = 'filesystem'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_ENGINE_OPTIONS = {'pool_recycle': 280}
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.mail.me.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
--- whatstodrink/whatstodrink/config_production.py
+++ whatstodrink/whatstodrink/config_production.py
@@ -1,9 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    
     SESSION_PERMANENT = False
     SESSION_TYPE = 'filesystem'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_ENGINE_OPTIONS = {'pool_recycle': 280}
     SQLALCHEMY_TRACK_MODIFICATIONS = False
