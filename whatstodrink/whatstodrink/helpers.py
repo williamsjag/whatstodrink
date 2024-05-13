6d6fd18e610609080e86b72c8ca1fe3f24014766
Jackson Williams <117761747+williamsjag@users.noreply.github.com>
2024-05-13 16:58:33 +0200

    cleanup

--- whatstodrink/whatstodrink/helpers.py
+++ whatstodrink/whatstodrink/helpers.py
@@ -20,7 +20,7 @@ WhatsToDrink.com"""
 
 def update_cocktail_recipes():
     # Retrieve all cocktails
-    all_cocktails = db.session.scalars(select(Cocktail.id).where(Cocktail.id > 199)).fetchall()
+    all_cocktails = db.session.scalars(select(Cocktail.id).where(chr(31) not in Cocktail.recipe)).fetchall()
 
     for cocktail_id in all_cocktails:
         # Execute the function for each cocktail
