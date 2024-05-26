39ebe47f1c3cb44b9ef136b3bfa518c1a40bb407
Jackson Williams <117761747+williamsjag@users.noreply.github.com>
2024-05-26 14:34:21 +0200

    Remove import update_cocktail from main

--- whatstodrink/whatstodrink/main/routes.py
+++ whatstodrink/whatstodrink/main/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, Blueprint
 from flask_login import current_user, login_required
-from whatstodrink.helpers import update_cocktail_recipes
+
 
 main = Blueprint('main', __name__)
 
